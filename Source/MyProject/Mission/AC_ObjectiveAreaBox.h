a48d300a33053da7869b1983f76739cd5612e290
Pete Allen <allenp@bournemouth.ac.uk>
2019-04-29 00:00:10 +0100

    Basic structure in, need player to have a current mission so it can be tracked.

---
+++ Source/MyProject/Mission/AC_ObjectiveAreaBox.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "Mission/ObjectiveTracker.h"
+#include "AC_ObjectiveAreaBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class MYPROJECT_API UAC_ObjectiveAreaBox : public UBoxComponent, public IObjectiveTracker
+{
+	GENERATED_BODY()
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	virtual bool IsObjectiveComplete() override;
+	
+	UFUNCTION()
+	void OverlapStart(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+
+	bool HasPlayerEnteredBox;
+};
---
+++ Source/MyProject/Mission/ObjectiveTracker.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ObjectiveTracker.h"
+
+
+// Add default functionality here for any IObjectiveTracker functions that are not pure virtual.
+
+bool IObjectiveTracker::IsObjectiveComplete()
+{
+	return false;
+}
